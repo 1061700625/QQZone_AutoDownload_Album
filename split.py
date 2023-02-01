1c1a3508eb2eda176bcf6ad29495a2cef7fcf9be
Songxf <1061700625@qq.com>
2023-02-02 01:40:42 +0800

    Add files via upload

--- split.py
+++ split.py
@@ -5,69 +5,116 @@ from tqdm import tqdm
 
 
 '''
-将指定文件夹下的文件，拆分为n份;
-如现有目录：
-- rootDir
--- dir1
----- file_1
----- file_2
-...
----- file_n
+瞎写写，大家随意看看
 
--- dir2
----- file_1
----- file_2
-...
----- file_n
+------------------------------------------
+对于split_files：
+    将指定文件夹下的文件，拆分为n份;
+    如现有目录：
+    - rootDir
+    -- dir1
+    ---- file_1
+    ---- file_2
+    ...
+    ---- file_n
 
-split_path_root = rootDir
-split_num = 2
+    -- dir2
+    ---- file_1
+    ---- file_2
+    ...
+    ---- file_n
 
-则运行后，目录变为：
-- rootDir
--- dir1
----- file_1
----- file_2
-...
----- file_n
-%新增部分-开始%
---- 1
----- file_1
----- file_2
---- 2
----- file_3
----- file_4
-...
-%新增部分-结束%
+    split_path_root = rootDir
+    split_num = 2
 
--- dir2
----- file_1
----- file_2
-...
----- file_n
-%新增部分-开始%
---- 1
----- file_1
----- file_2
---- 2
----- file_3
----- file_4
-...
-%新增部分-结束%
+    则运行后，目录变为：
+    - rootDir
+    -- dir1
+    ---- file_1
+    ---- file_2
+    ...
+    ---- file_n
+    %新增部分-开始%
+    --- 1
+    ---- file_1
+    ---- file_2
+    --- 2
+    ---- file_3
+    ---- file_4
+    ...
+    %新增部分-结束%
+
+    -- dir2
+    ---- file_1
+    ---- file_2
+    ...
+    ---- file_n
+    %新增部分-开始%
+    --- 1
+    ---- file_1
+    ---- file_2
+    --- 2
+    ---- file_3
+    ---- file_4
+    ...
+    %新增部分-结束%
+
+------------------------------------------
+对于split_files2：
+
+    如现有目录：
+    - rootDir
+    ---- file_1
+    ---- file_2
+    ...
+    ---- file_n
+
+    split_path_root = rootDir
+    split_num = 2
+
+    则运行后，目录变为：
+    - rootDir
+    ---- file_1
+    ---- file_2
+    ...
+    ---- file_n
+    %新增部分-开始%
+    --- 1
+    ---- file_1
+    ---- file_2
+    --- 2
+    ---- file_3
+    ---- file_4
+    ...
+    %新增部分-结束%
 '''
 
 
-# 待拆分的文件夹路径
-split_path_root = r'C:\Users\SXF\Desktop\新建文件夹'
-# 每份文件夹里多少个文件
-split_num = 500
+def split_files(split_path_root, split_num):
+    for item in os.listdir(split_path_root):
+        dir_path = os.path.join(split_path_root, item)
+        if not os.path.isdir(dir_path):
+            continue
+        print(dir_path)
+        img_lists = list(filter(lambda x: os.path.isfile(os.path.join(dir_path, x)), os.listdir(dir_path)))
+        split_cnt = math.ceil(len(img_lists)/split_num)
+        for i in range(split_cnt):
+            index_start = i * split_num
+            index_end = (i+1) * split_num
+            sub_dir_path = os.path.join(dir_path, str(i))
+            if not os.path.exists(sub_dir_path):
+                os.mkdir(sub_dir_path)
+            for file_name in tqdm(img_lists[index_start: index_end]):
+                src_file_path = os.path.join(dir_path, file_name)
+                dst_file_path = os.path.join(sub_dir_path, file_name)
+                if not os.path.exists(dst_file_path):
+                    shutil.copy(src_file_path, dst_file_path)
+    print('done!')
 
 
-for item in os.listdir(split_path_root):
-    dir_path = os.path.join(split_path_root, item)
-    if not os.path.isdir(dir_path):
-        continue
-    print(dir_path)
+def split_files2(split_path_root, split_num):
+    result_dir_path = []
+    dir_path = split_path_root
     img_lists = list(filter(lambda x: os.path.isfile(os.path.join(dir_path, x)), os.listdir(dir_path)))
     split_cnt = math.ceil(len(img_lists)/split_num)
     for i in range(split_cnt):
@@ -76,13 +123,19 @@ for item in os.listdir(split_path_root):
         sub_dir_path = os.path.join(dir_path, str(i))
         if not os.path.exists(sub_dir_path):
             os.mkdir(sub_dir_path)
+        result_dir_path.append(sub_dir_path)
         for file_name in tqdm(img_lists[index_start: index_end]):
             src_file_path = os.path.join(dir_path, file_name)
             dst_file_path = os.path.join(sub_dir_path, file_name)
             if not os.path.exists(dst_file_path):
                 shutil.copy(src_file_path, dst_file_path)
-print('done!')
+    print('done!')
+    return result_dir_path
 
-
-
+if __name__ == "__main__":
+    # 待拆分的文件夹路径
+    split_path_root = r'C:\Users\SXF\Desktop\test\1-1000'
+    # 每份文件夹里多少个文件
+    split_num = 500
+    split_files2(split_path_root, split_num)
 
